e08d9c44e298a865da9287fc0ca2aa87a7ad122f
anuradhapathak08 <anuradhapathak699@gmail.com>
2024-09-30 13:01:36 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,5 +1,17 @@
 def decode_message( s: str, p: str) -> bool:
-
-# write your code here
-  
+def match(s: str, p: str, s_index: int, p_index: int) -> bool:
+        if s_index == len(s) and p_index == len(p):
+     return True
+        if p_index == len(p):
+            return False
+        
+        if p[p_index] == '*':
+            return match(s, p, s_index, p_index + 1) or \
+                   (s_index < len(s) and match(s, p, s_index + 1, p_index))
+        
+        if s_index < len(s) and (s[s_index] == p[p_index] or p[p_index] == '?'):
+            return match(s, p, s_index + 1, p_index + 1)
+        
         return False
+
+    return match(s, p, 0, 0)
